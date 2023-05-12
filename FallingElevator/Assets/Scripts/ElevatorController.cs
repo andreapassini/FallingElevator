8a4c648d08b4fbb4bf129700d08f3bab234449c5
Andrea Passini <andreapa98@gmail.com>
2023-05-12 15:05:51 +0200

    Elevator movement

--- FallingElevator/Assets/Scripts/ElevatorController.cs
+++ FallingElevator/Assets/Scripts/ElevatorController.cs
@@ -7,11 +7,18 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class ElevatorController : MonoBehaviour
 {
+    public float minForce = 10f;
+    public float maxForce = 25f;
+    
     private Rigidbody2D _rigidbody2D;
     private Collider2D _collider2D;
 
     private bool _moveRight = false;
     private Vector2 _vecForce;
+    private bool _directionChanged = false;
+
+    private float _increment;
+    public float step = .5f;
     
     private void Awake()
     {
@@ -24,25 +31,38 @@ public class ElevatorController : MonoBehaviour
         // Catch the input
         if (Input.GetKey(KeyCode.Space))
         {
+            if (!_moveRight)
+            {
+                _directionChanged = true;
+                Debug.Log("Left to Right");
+            }
+            else
+            {
+                _directionChanged = false;
+            }
             _moveRight = true;
+            _vecForce = Vector2.right * InterpolateForce(_directionChanged);
         }
         else
         {
+            if (_moveRight)
+            {
+                _directionChanged = true;
+                Debug.Log("Right to Left");
+            }
+            else
+            {
+                _directionChanged = false;
+            }
+            
             _moveRight = false;
+            _vecForce = Vector2.left * InterpolateForce(_directionChanged);
         }
     }
 
     private void FixedUpdate()
     {
-        if (_moveRight)
-        {
-            _vecForce = Vector2.right;
-        }
-        else
-        {
-            _vecForce = Vector2.left;
-        }
-        
+        Debug.Log(_vecForce.x);
         // Add the forces to the rb
         _rigidbody2D.AddForce(_vecForce);
     }
@@ -53,5 +73,17 @@ public class ElevatorController : MonoBehaviour
         {
             hit.Hit(transform);
         }
+    }
+
+    private float InterpolateForce(bool reset)
+    {
+        if (reset)
+        {
+            _increment = 0f;
+            return minForce;
+        }
+
+        _increment += step * Time.fixedDeltaTime;
+        return Mathf.Lerp(minForce, maxForce, _increment);
     }
 }
